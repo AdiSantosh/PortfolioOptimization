81714ef983253a00e7025f5471926ca599aef914
rajeevsurve06 <rajeevsurve8@gmail.com>
2024-09-21 22:50:27 -0400

    Added min VAriance portfolio strategy

--- src/algorithms/strategy.py
+++ src/algorithms/strategy.py
@@ -48,7 +48,7 @@ class ConstrainedBasedStrategy(Strategy):
             
             rtn_data = price_data.pct_change()[1:]
 
-            wealth_allocations = self.get_optimal_allocations(rtn_data.T.iloc[:,1:],1)
+            wealth_allocations = self.get_optimal_allocations(rtn_data.T,1)
             weights_dict[date] = dict(zip(price_data.columns,wealth_allocations))
 
         weights_dict = pd.DataFrame(weights_dict).T
@@ -59,7 +59,7 @@ class ConstrainedBasedStrategy(Strategy):
 class CvarMretOpt(ConstrainedBasedStrategy):
 
 
-    def __init__(self,ratio=0.5,risk_level=0.3):
+    def __init__(self,ratio=0.5,risk_level=0.1):
 
 
         self.ratio = ratio
@@ -105,11 +105,24 @@ class CvarMretOpt(ConstrainedBasedStrategy):
         lhs_eq[0,self.num_senarios:self.num_senarios+self.num_assets] = 1
         rhs_eq = [self.investment_amount]
 
+        # bnd = []
+        # for i in range(self.num_senarios):
+        #     bnd.append((0,float('inf')))
+
+        # for i in range(self.num_assets+1):
+        #     bnd.append((float("-inf"),float('inf')))
+
+        # bnd[-1] = (float('-inf'),float('inf'))
+
         bnd = []
-        for i in range(self.num_senarios+self.num_assets+1):
+        for i in range(self.num_senarios):
             bnd.append((0,float('inf')))
 
+        for i in range(self.num_assets+1):
+            bnd.append((0,float('inf')))
+            
         bnd[-1] = (float('-inf'),float('inf'))
+
 
         obj = np.ones((1,self.num_senarios+self.num_assets+1))*(1/risk_level)*(1/self.num_senarios)*(ratio)
         obj[0,-1] = -1*(ratio)
@@ -174,8 +187,7 @@ class MeanSemidevOpt(ConstrainedBasedStrategy):
 
         opt = optimize.linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq,A_eq=lhs_eq,b_eq=rhs_eq, bounds=bnd,method="highs")
         return opt
-        
-
+    
 
 class EqualyWeighted(ConstrainedBasedStrategy):
 
@@ -195,3 +207,254 @@ class EqualyWeighted(ConstrainedBasedStrategy):
         self.num_assets = len(self.array[:,0])
         return (np.ones(self.num_assets)/self.num_assets)*investment_amount
     
+
+
+class MeanSemiEquallyWeighted(ConstrainedBasedStrategy):
+
+    def __init__(self,ratio):
+       
+        self.ratio = ratio 
+        self.results = None
+        self.array = None
+        self.num_assets = None
+        self.num_senarios = None
+        self.array_transpose = None
+        self.investment_amount = None
+        self.results = None
+        
+    def get_optimal_allocations(self,returns_data:pd.DataFrame,investment_amount:int=1):
+
+        self.array = returns_data.iloc[:, 1:].to_numpy()
+        self.num_assets = len(self.array[:,0])
+        self.num_senarios = len(self.array[0,:])
+        self.array_transpose = np.transpose(self.array)
+        self.investment_amount = 1
+        self.results = self.optimize(self.ratio)
+        allocations = np.array(self.results.x[self.num_senarios:self.num_senarios+self.num_assets])
+        return (allocations + (np.ones(self.num_assets)/self.num_assets))/(np.sum(allocations) + (np.sum((np.ones(self.num_assets)/self.num_assets))))
+
+    def optimize(self,ratio):
+
+        """
+            ρ =  Hybrid Risk Measure
+            ρ = [Z(x)] = -E[Z(x)] + c σ[Z(x)]
+
+            where σ[Z] = E{ max(0,E[Z] – Z)} is the lower semideviation of the first order.
+        """
+
+        mean = np.resize(self.array.mean(axis=1),(self.num_assets,1))
+        lhs_ineq = np.zeros((self.num_senarios,self.num_senarios+self.num_assets))
+
+        for i in range(self.num_senarios):
+            lhs_ineq[i,i] = -1  # vk
+            lhs_ineq[i,self.num_senarios:self.num_senarios+self.num_assets] = -1*(self.array[:,i]) + np.transpose(mean)  # Rk
+
+        rhs_ineq = np.zeros((1,self.num_senarios))
+
+        lhs_eq = np.zeros((1,self.num_senarios+self.num_assets))
+        lhs_eq[0,self.num_senarios:self.num_senarios+self.num_assets] = 1
+        rhs_eq = [self.investment_amount]
+
+        bnd = []
+        for i in range(self.num_senarios+self.num_assets):
+            bnd.append((0,float('inf')))
+
+        obj = np.ones((1,self.num_senarios+self.num_assets))*(1/self.num_senarios)*ratio
+        obj[0,self.num_senarios:self.num_senarios+self.num_assets] = -1*np.array(np.transpose(mean))
+
+        opt = optimize.linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq,A_eq=lhs_eq,b_eq=rhs_eq, bounds=bnd,method="highs")
+        return opt
+
+    
+
+
+class StochasticDominant(ConstrainedBasedStrategy):
+
+    def __init__(self,ratio):
+       
+        self.ratio = ratio
+        self.results = None
+        self.array = None
+        self.num_assets = None
+        self.num_senarios = None
+        self.array_transpose = None
+        self.investment_amount = None
+        self.results = None
+        
+    def get_optimal_allocations(self,returns_data:pd.DataFrame,investment_amount:int=1):
+
+        self.array = returns_data.iloc[:, 1:].to_numpy()
+        self.num_assets = len(self.array[:,0])
+        self.num_senarios = len(self.array[0,:])
+        self.array_transpose = np.transpose(self.array)
+        self.investment_amount = investment_amount
+        try:
+            self.results = self.optimize()
+            allocations = np.array(np.array(self.results.x)[:self.num_assets])
+            return (allocations + (np.ones(self.num_assets)/self.num_assets))/(np.sum(allocations) + (np.sum((np.ones(self.num_assets)/self.num_assets))))
+        except:
+            self.array = returns_data.iloc[:, 1:].to_numpy()
+            self.num_assets = len(self.array[:,0])
+            self.num_senarios = len(self.array[0,:])
+            self.array_transpose = np.transpose(self.array)
+            self.investment_amount = 1
+            self.results = self.optimize(self.ratio)
+            allocations = np.array(self.results.x[self.num_senarios:self.num_senarios+self.num_assets])
+            return (allocations + (np.ones(self.num_assets)/self.num_assets))/(np.sum(allocations) + (np.sum((np.ones(self.num_assets)/self.num_assets))))
+
+
+    def optimize(self):
+
+        # """
+        #     ρ =  Hybrid Risk Measure
+        #     ρ = [Z(x)] = -E[Z(x)] + c σ[Z(x)]
+
+        #     where σ[Z] = E{ max(0,E[Z] – Z)} is the lower semideviation of the first order.
+        # """
+
+        equally_weighted = (np.ones((1,self.num_assets))/self.num_assets)[0,:]
+        values = equally_weighted@self.array
+
+        eq_dict = sorted(dict(zip(values,self.array.T)).items())
+
+        lhs_ineq = np.zeros((self.num_senarios , self.num_senarios+self.num_assets))
+        rhs_ineq = np.zeros((1,self.num_senarios))
+
+        for ind,dict_item in enumerate(eq_dict):
+            loss,senario = dict_item
+            lhs_ineq[ind, :self.num_assets] = -senario
+            lhs_ineq[ind, self.num_assets+ind] = +1
+            if loss<0:
+                rhs_ineq[:,ind] = -loss
+            else:
+                rhs_ineq[:,ind] = -np.array((sorted(values[values>0])[0]))
+
+        rhs_ineq = np.zeros((1,self.num_senarios))
+
+        lhs_eq = np.zeros((1,self.num_senarios+self.num_assets))
+        lhs_eq[0,:self.num_assets] = 1
+        rhs_eq = [self.investment_amount]
+
+        bnd = []
+        for i in range(self.num_assets):
+            bnd.append((float("-inf"),float('inf')))
+
+        for i in range(self.num_senarios):
+            bnd.append((float("0"),float('inf')))
+
+        obj = np.zeros((1,self.num_senarios+self.num_assets))
+        obj[0,self.num_assets:] = -1
+
+        opt = optimize.linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq,A_eq=lhs_eq,b_eq=rhs_eq, bounds=bnd,method="highs")
+        return opt
+
+
+    def optimize_2(self,ratio):
+
+        """
+            ρ =  Hybrid Risk Measure
+            ρ = [Z(x)] = -E[Z(x)] + c σ[Z(x)]
+
+            where σ[Z] = E{ max(0,E[Z] – Z)} is the lower semideviation of the first order.
+        """
+
+        mean = np.resize(self.array.mean(axis=1),(self.num_assets,1))
+        lhs_ineq = np.zeros((self.num_senarios,self.num_senarios+self.num_assets))
+
+        for i in range(self.num_senarios):
+            lhs_ineq[i,i] = -1  # vk
+            lhs_ineq[i,self.num_senarios:self.num_senarios+self.num_assets] = -1*(self.array[:,i]) + np.transpose(mean)  # Rk
+
+        rhs_ineq = np.zeros((1,self.num_senarios))
+
+        lhs_eq = np.zeros((1,self.num_senarios+self.num_assets))
+        lhs_eq[0,self.num_senarios:self.num_senarios+self.num_assets] = 1
+        rhs_eq = [self.investment_amount]
+
+        bnd = []
+        for i in range(self.num_senarios+self.num_assets):
+            bnd.append((0,float('inf')))
+
+        obj = np.ones((1,self.num_senarios+self.num_assets))*(1/self.num_senarios)*ratio
+        obj[0,self.num_senarios:self.num_senarios+self.num_assets] = -1*np.array(np.transpose(mean))
+
+        opt = optimize.linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq,A_eq=lhs_eq,b_eq=rhs_eq, bounds=bnd,method="highs")
+        return opt
+
+class MeanVariance(ConstrainedBasedStrategy):
+
+    
+    def __init__(self,target_return=None,allow_shorting=False):
+
+        self.results = None
+        self.array = None
+        self.num_assets = None
+        self.num_senarios = None
+        self.array_transpose = None
+        self.investment_amount = None
+        self.target_return =  target_return
+        self.allow_shorting = allow_shorting
+        
+    def calculate_mean(self) -> pd.Series:
+        mean_returns = self.returns.mean()
+        return mean_returns
+
+    def calculate_covariance_matrix(self) -> pd.DataFrame:
+        covariance_matrix = self.returns.cov()
+        return covariance_matrix
+
+    def portfolio_variance(self, weights):
+        return np.dot(weights.T, np.dot(self.cov_matrix, weights))
+    
+    def min_variance(self, allow_shorting=False):
+        num_assets = len(self.mean_returns)
+        initial_weights = np.ones(num_assets) / num_assets
+        constraints = [{'type': 'eq', 'fun': lambda x: np.sum(x) - 1}]
+
+        if allow_shorting:
+            bounds = tuple((float('-inf'), float('inf')) for _ in range(num_assets))  
+        else:
+            bounds = tuple((0, float('inf')) for _ in range(num_assets))  
+        
+        result = optimize.minimize(self.portfolio_variance, initial_weights, method='SLSQP', bounds=bounds, constraints=constraints)
+        return result
+    
+    def min_variance_allocation(self):
+        
+        result = self.min_variance()
+        if result.success:
+            return dict(zip(self.returns.columns, result.x))
+        else:
+            raise ValueError("Optimization failed: " + result.message)
+
+    def get_max_return(self) -> float:
+        return self.mean_returns.max()
+
+    def minimize_func(self, weights):
+        return np.matmul(np.matmul(np.transpose(weights), self.cov_matrix), weights)
+    
+    def optimize(self, target_return=None, allow_shorting=False):
+        
+        num_assets = len(self.mean_returns)
+        constraints = [{'type': 'eq', 'fun': lambda weights: np.sum(weights) - 1}]
+        if target_return is not None:
+            constraints.append({'type': 'eq', 'fun': lambda weights: np.dot(weights, self.mean_returns) - target_return})
+        if allow_shorting:
+            bounds = tuple((float('-inf'), float('inf')) for _ in range(num_assets))  
+        else:
+            bounds = tuple((0, float('inf')) for _ in range(num_assets))  
+        initial_weights = np.ones(num_assets) / num_assets
+        result = optimize.minimize(self.minimize_func, initial_weights, method='SLSQP', bounds=bounds, constraints=constraints)
+        return result
+
+    def get_optimal_allocations(self,returns_data:pd.DataFrame,investment_amount:int=1):
+
+        self.returns =  returns_data.iloc[:, 1:]
+        self.mean_returns = self.calculate_mean().to_numpy()
+        self.cov_matrix = self.calculate_covariance_matrix().to_numpy()
+
+        result = self.optimize(target_return=self.target_return,allow_shorting=self.allow_shorting)
+        if result.success:
+            return result.x
+        else:
+            raise ValueError("Optimization failed: " + result.message)
